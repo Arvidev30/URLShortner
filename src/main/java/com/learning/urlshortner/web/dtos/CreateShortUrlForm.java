48df29abf2c1982749156896ebacfb242faf677d
Arvidev30 <arvidevacc@gmail.com>
2025-06-11 20:53:20 +0530

    Created a record to store the error message using the jakarta validation

--- src/main/java/com/learning/urlshortner/web/dtos/CreateShortUrlForm.java
+++ src/main/java/com/learning/urlshortner/web/dtos/CreateShortUrlForm.java
@@ -1,4 +1,8 @@
 package com.learning.urlshortner.web.controllers.dtos;
 
-public class CreateShortUrlForm {
+import jakarta.validation.constraints.NotBlank;
+
+public record CreateShortUrlForm(
+        @NotBlank(message = "Original URL is required")
+        String originalUrl) {
 }
