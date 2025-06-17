35743e417b14bd0b49347c778824f4b82c7c223f
Arvidev30 <arvidevacc@gmail.com>
2025-06-18 01:56:44 +0530

    Redirect to Original URL when using short URL

--- src/main/java/com/learning/urlshortner/web/controllers/HomeController.java
+++ src/main/java/com/learning/urlshortner/web/controllers/HomeController.java
@@ -1,9 +1,10 @@
 package com.learning.urlshortner.web.controllers;
 
+import com.learning.urlshortner.domain.exception.ShortURLNotFoundException;
 import com.learning.urlshortner.domain.models.CreateShortUrlCmd;
 import com.learning.urlshortner.domain.models.ShortUrlDTO;
 import com.learning.urlshortner.domain.services.ShortURLService;
-import com.learning.urlshortner.web.ApplicationProperties;
+import com.learning.urlshortner.ApplicationProperties;
 import com.learning.urlshortner.web.dtos.CreateShortUrlForm;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
@@ -11,10 +12,12 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class HomeController {
@@ -46,7 +49,7 @@ public class HomeController {
     }
 
     @PostMapping("/short-urls")
-    public String createShortUrl(@ModelAttribute("createShortUrlForm") @Valid CreateShortUrlForm form,
+    String createShortUrl(@ModelAttribute("createShortUrlForm") @Valid CreateShortUrlForm form,
                                  BindingResult bindingResult,
                                  RedirectAttributes redirectAttributes,
                                  Model model){
@@ -68,6 +71,16 @@ public class HomeController {
             redirectAttributes.addFlashAttribute("errorMessage", "Failed to create the short URL");
         }
         return "redirect:/";
+    }
+
+    @GetMapping("/s/{shortKey}")
+    String redirectToOriginalUrl(@PathVariable String shortKey){
+        Optional<ShortUrlDTO> shortUrlDTOOptional = shortURLService.accessShortUrl(shortKey);
+        if(shortUrlDTOOptional.isEmpty()){
+            throw new ShortURLNotFoundException("Invalid Short Key: "+shortKey);
+        }
+        ShortUrlDTO shortUrlDTO = shortUrlDTOOptional.get();
+        return "redirect:"+shortUrlDTO.originalUrl();
     }
 
 }
