273eebc83852c38c49d50c7c67f120030eddaa7a
Arvidev30 <arvidevacc@gmail.com>
2025-06-25 22:51:41 +0530

    Created a new web security filter chain to map the pages visible to the logged in, logged out user and with the corresponding roles.

---
+++ src/main/java/com/learning/urlshortner/config/WebSecurity.java
@@ -0,0 +1,45 @@
+package com.learning.urlshortner.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurity {
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http
+                .csrf(CsrfConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(
+                                "/error", "/", "webjars/**", "/css/**", "/login",
+                                "/js/**", "/images/**", "/short-urls", "/s/**", "/register"
+                        ).permitAll()
+                        .requestMatchers("my-urls").authenticated()
+                        .requestMatchers("/admin/**").authenticated()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/")
+                        .permitAll()
+                )
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/login?logout")
+                        .permitAll()
+                );
+        return http.build();
+    }
+}
