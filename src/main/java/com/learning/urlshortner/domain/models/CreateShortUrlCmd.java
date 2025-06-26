1d7fc7246fa58a1aa52eb5216489bf585bd5f3eb
Arvidev30 <arvidevacc@gmail.com>
2025-06-26 20:00:12 +0530

    added parameters like expiration, isprivate and used id

--- src/main/java/com/learning/urlshortner/domain/models/CreateShortUrlCmd.java
+++ src/main/java/com/learning/urlshortner/domain/models/CreateShortUrlCmd.java
@@ -1,4 +1,7 @@
 package com.learning.urlshortner.domain.models;
 
-public record CreateShortUrlCmd(String originalUrl) {
+public record CreateShortUrlCmd(String originalUrl,
+                                Integer expirationInDays,
+                                Boolean isPrivate,
+                                Long userId) {
 }
