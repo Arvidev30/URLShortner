668333ae5818e05e65da7727ce8298ed5a879045
Arvidev30 <arvidevacc@gmail.com>
2025-06-26 17:40:49 +0530

    new class to get the current user when logged in or else null when using as guest

---
+++ src/main/java/com/learning/urlshortner/web/controllers/SecurityUtils.java
@@ -0,0 +1,32 @@
+package com.learning.urlshortner.web.controllers;
+
+import com.learning.urlshortner.domain.entities.User;
+import com.learning.urlshortner.domain.repositories.UserRepository;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SecurityUtils {
+
+    private final UserRepository userRepository;
+
+    public SecurityUtils(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User getCurrentUser(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+     //   Checks for null, anonymous, or unauthenticated
+        if (authentication == null ||
+                !authentication.isAuthenticated() ||
+                authentication instanceof AnonymousAuthenticationToken) {
+            return null;
+        }
+
+        String email = authentication.getName();
+
+        return userRepository.findByEmail(email).orElseThrow();
+    }
+}
