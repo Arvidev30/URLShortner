c1a92c88b89eec2b7b31831dfc905bad9c96504f
Arvidev30 <arvidevacc@gmail.com>
2025-06-26 20:00:36 +0530

    added parameters like expiration and isprivate

--- src/main/java/com/learning/urlshortner/web/dtos/CreateShortUrlForm.java
+++ src/main/java/com/learning/urlshortner/web/dtos/CreateShortUrlForm.java
@@ -1,9 +1,15 @@
 package com.learning.urlshortner.web.dtos;
 
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 
 public record CreateShortUrlForm(
         @NotBlank(message = "Original URL is required")
-        String originalUrl) {
+        String originalUrl,
+        @Min(1)
+        @Max(365)
+        Integer expirationInDays,
+        Boolean isPrivate) {
 }
